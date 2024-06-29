ffb5b12845da4c0d41dc4e8172d3cf230289b61b
Jacob Pitkin <jacob.pitkin@powin.com>
2024-06-29 14:43:40 -0700

    adding calendar DI

---
+++ src/main/java/com/jacobpitkin/assistant/calendar/CalendarController.java
@@ -0,0 +1,25 @@
+package com.jacobpitkin.assistant.calendar;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("calendar")
+public class CalendarController {
+    private final CalendarService calendarService;
+
+    public CalendarController(CalendarService calendarService) {
+        this.calendarService = calendarService;
+    }
+    
+    @GetMapping("/")
+    public String index() {
+        return "This is the calendar endpoint.";
+    }
+
+    @GetMapping("/service")
+    public String getService() {
+        return calendarService.getFromService();
+    }
+}
---
+++ src/main/java/com/jacobpitkin/assistant/calendar/CalendarService.java
@@ -0,0 +1,10 @@
+package com.jacobpitkin.assistant.calendar;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalendarService {
+    public String getFromService() {
+        return "This message is from the calendar service.";
+    }
+}
